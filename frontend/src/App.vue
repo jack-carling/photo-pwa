8538b5a6819f0080ad78effe8fbf05cac9eb57b6
jensengbg-jack-carling <72305598+jensengbg-jack-carling@users.noreply.github.com>
2021-05-14 12:27:24 +0200

    Fixed grid-layout
    Responsive, auto-adjusting to content and also height of navigation components.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,21 @@
 <template>
-<main>
-  <NavigationTop />
-    <router-view></router-view>
-  <NavigationBottom />
-</main>
+  <main class="app">
+    <NavigationTop />
+      <router-view id="router"></router-view>
+    <NavigationBottom />
+  </main>
 </template>
 
 <script>
-import NavigationTop from './components/NavigationTop.vue'
-import NavigationBottom from './components/NavigationBottom.vue'
+import NavigationTop from './components/NavigationTop.vue';
+import NavigationBottom from './components/NavigationBottom.vue';
 
 export default {
   components: {
     NavigationTop,
-    NavigationBottom
-  }
-}
+    NavigationBottom,
+  },
+};
 </script>
 
 <style>
@@ -25,5 +25,16 @@ export default {
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+#router {
+  max-height: 100%;
+  margin: 0;
+  padding: 1rem;
+  overflow: auto;
+}
+main.app {
+  display: grid;
+  grid-template-rows: min-content auto min-content;
+  height: 100vh;
 }
 </style>
--- frontend/src/components/NavigationBottom.vue
+++ frontend/src/components/NavigationBottom.vue
@@ -11,4 +11,8 @@ export default {
 </script>
 
 <style scoped>
+div {
+  height: 50px;
+  border-top: 1px solid #CCCCCC;
+}
 </style>
