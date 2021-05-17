2d1a88ea65599d06b68bbca6e72e82458ffb8576
jensengbg-Marijune-Lejsund <59868177+jensengbg-Marijune-Lejsund@users.noreply.github.com>
2021-05-17 17:03:55 +0200

    Update NavigationTop.vue
    tried the back button

--- frontend/src/components/NavigationTop.vue
+++ frontend/src/components/NavigationTop.vue
@@ -1,18 +1,26 @@
 <template>
-  <div>
-    NavigationTop
+  <div class="cyan darken-1">
+    <i class="material-icons" @click="goBack()">reply</i>
   </div>
 </template>
 
 <script>
-export default {
+export default {};
 
+function goBack() {
+  window.history.back();
 }
 </script>
 
 <style scoped>
 div {
   height: 50px;
-  border-bottom: 1px solid #CCCCCC;
+  border-bottom: 1px solid #cccccc;
+}
+
+i {
+  color: white;
+  font-size: 3rem;
+  cursor: pointer;
 }
 </style>
