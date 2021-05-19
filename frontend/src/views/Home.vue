4aae09c2ef20c1593070b097978335aaab9356d8
Padma <padmaprogrammer@gmail.com>
2021-05-19 15:50:40 +0200

    Homepage updates
    Pair programming:
    Victoria Graf
    Padma Chingunde
    
    Note: need to fix the padding on the grid.

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,23 +1,91 @@
 <template>
-  <main>
-    <h1>Home</h1>
-    <span>{{ name }}</span>
-  </main>
+  <body>
+    <section class="hero-banner">
+      <h1>Home</h1>
+      <div class="homepage-elements">
+        <img class="dark-logo" src="../assets/Apperture_dark_logo.svg" />
+        <a class="waves-effect cyan darken-1 btn">Login</a>
+        <a class="waves-effect cyan darken-1 btn">Create Account</a>
+      </div>
+      <div class="row">
+        <div class="col s6 m6 14">
+          <img
+            class="image_1"
+            src="../assets/pexels-jaime-reimer-2662086.jpg"
+          />
+        </div>
+        <div class="col s6 m6 14">
+          <img
+            class="image_2"
+            src="../assets/pexels-alexandr-podvalny-2070485.jpg"
+          />
+        </div>
+        <div class="col s12 m6 14">
+          <img class="image_3" src="../assets/pexels-jimmy-teoh-1010657.jpg" />
+        </div>
+        <div class="col s6 m6 14">
+          <img class="image_4" src="../assets/pexels-david-bartus-586687.jpg" />
+        </div>
+        <div class="col s6 m6 14">
+          <img class="image_5" src="../assets/pexels-jess-vide-4319752.jpg" />
+        </div>
+        <div class="col s12 m6 14">
+          <img
+            class="image_6"
+            src="../assets/pexels-nina-uhlíková-287240.jpg"
+          />
+        </div>
+        <div class="col s12 m6 14">
+          <img class="image_6" src="../assets/pexels-esrageziyor-7472287.jpg" />
+        </div>
+      </div>
+    </section>
+  </body>
 </template>
-
+  
 <script>
 export default {
   computed: {
-    name() {
-      if (!this.$store.state.user.online) {
-        return 'Welcome to Appenture!';
-      } else {
-        const name = this.$store.state.user.name;
-        return `Welcome back ${name}!`;
-      }
+    online() {
+      return this.$store.state.user.online;
     },
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+html {
+  max-width: 100vw;
+}
+
+#router {
+  margin: 0;
+  padding: 0;
+}
+
+.hero-banner {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  background-image: url("../assets/homepage_hero.svg");
+  background-size: contain;
+  background-repeat: no-repeat;
+}
+
+.homepage-elements {
+  margin-top: 19rem;
+  height: 9rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+img {
+  width: 100%;
+}
+
+.row {
+  margin-top: 10%;
+}
+</style>
