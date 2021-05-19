e7a05db574c17f81f77dd6b6a0ad91be4676c44c
jensengbg-jack-carling <72305598+jensengbg-jack-carling@users.noreply.github.com>
2021-05-19 16:35:03 +0200

    Update NavigationTop.vue

--- frontend/src/components/NavigationTop.vue
+++ frontend/src/components/NavigationTop.vue
@@ -1,26 +1,47 @@
 <template>
-  <div class="cyan darken-1">
-    <i class="material-icons" @click="goBack()">reply</i>
+  <div class="main cyan darken-1">
+    <div v-if="chat" @click="$router.go(-1)">
+      <i class="material-icons">arrow_back</i>
+    </div>
+    <div v-else></div>
+    <img @click="$router.push('/')" src="../assets/Apperture_white_logo.svg" alt="" />
   </div>
 </template>
 
 <script>
-export default {};
-
-function goBack() {
-  window.history.back();
-}
+export default {
+  computed: {
+    chat() {
+      if (this.$route.path === '/chat') {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
-div {
+div.main {
   height: 50px;
-  border-bottom: 1px solid #cccccc;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
-
+div.main div {
+  width: 50px;
+  height: 50px;
+  display: grid;
+  place-items: center;
+}
 i {
-  color: white;
-  font-size: 3rem;
+  font-size: 2.5rem;
+  color: #eceff1;
   cursor: pointer;
+}
+img {
+  margin-right: 0.5rem;
+  justify-self: end;
 }
 </style>
