89c99d874de2a34883797e917a0c27005a48ece6
jensengbg-jack-carling <72305598+jensengbg-jack-carling@users.noreply.github.com>
2021-05-20 09:35:37 +0200

    Bug fix
    Used same name for computed property as data variable (Account.vue)
    
    Moved chat to module

--- backend/server.js
+++ backend/server.js
@@ -11,38 +11,7 @@ const { express, app } = require('mongoosy')({
 
 app.use(express.json());
 
-let connections = [];
-
-app.get('/api/chat', (req, res) => {
-  let connection = { req, res };
-  connections.push(connection);
-
-  req.on('close', () => {
-    connections = connections.filter((x) => x !== connection);
-  });
-
-  res.set({
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-  });
-
-  /* Avoid Vite http proxy error */
-  const message = JSON.stringify({ initial: true });
-  res.write('data: ' + message + ' \n\n');
-});
-
-app.post('/api/chat/message', (req, res) => {
-  let message = req.body;
-  message.time = Date.now();
-  emitMessage(message);
-  res.json({ success: true });
-});
-
-function emitMessage(message) {
-  for (let { res } of connections) {
-    res.write('data: ' + JSON.stringify(message) + '\n\n');
-  }
-}
+require('./chat')(app);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
