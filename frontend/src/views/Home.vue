e4eb247aca571c44ae409b3741b19b04c3f98b5e
Padma <padmaprogrammer@gmail.com>
2021-05-23 18:57:43 +0200

    Update Home.vue
    Homepage updates: dynamic grid renders images from database

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,29 @@
 <template>
-  <main-container>
+  <div class="main-container">
     <section class="hero-banner">
-      <img class="homepage-hero" src="../assets/homepage-hero-gradient.png" alt="homepage-hero">
-        <form>
-          <div class="input-field">
-            <input id="search" type="search" required placeholder="Search for tags, locations and people">
-            <label class="label-icon" for="search"><i class="material-icons" @click="$router.push('/search')">search</i></label>
-          </div>
-        </form>
+      <img
+        class="homepage-hero"
+        src="../assets/homepage-hero-gradient.png"
+        alt="homepage-hero"
+      />
+      <form>
+        <div class="input-field">
+          <input
+            id="search"
+            type="search"
+            required
+            placeholder="Search for tags, locations and people"
+          />
+          <label class="label-icon" for="search"
+            ><i class="material-icons" @click="$router.push('/search')"
+              >search</i
+            ></label
+          >
+        </div>
+      </form>
       <div class="homepage-elements">
         <img class="dark-logo" src="../assets/Apperture_dark_logo.svg" />
+<<<<<<< Updated upstream
         <a class="waves-effect waves-light cyan darken-1 btn" @click="$router.push('/account')">Login</a>
         <a class="waves-effect waves-light cyan darken-1 btn" @click="$router.push('/account?redirect=signup')">Sign up</a>
       </div>
@@ -46,29 +60,75 @@
             <img class="image_6" src="../assets/pexels-esrageziyor-7472287.jpg" />
           </div>
         </div>
+=======
+        <a class="waves-effect waves-light cyan darken-1 btn">Login</a>
+        <a
+          class="waves-effect waves-light cyan darken-1 btn"
+          @click="$router.push('/register')"
+          >Sign up</a
+        >
+>>>>>>> Stashed changes
       </div>
     </section>
-  </main-container>
+    <section class="images">
+      <img
+        :src="upload.url"
+        v-for="(upload, index) in uploads"
+        v-bind:key="index"
+      />
+    </section>
+  </div>
 </template>
   
 <script>
+import mongoosy from "mongoosy/frontend";
+const { Upload } = mongoosy;
+
 export default {
+  data() {
+    return {
+      uploads: [],
+    };
+  },
   computed: {
     online() {
       return this.$store.state.user.online;
     },
   },
+
+  async mounted() {
+    const uploads = await Upload.find();
+    this.uploads = uploads;
+  },
 };
 </script>
 
+
+
 <style scoped>
-html {
-  max-width: 100vw;
+* {
+  overflow: hidden;
+  box-sizing: border-box;
 }
 
 #router {
   margin: 0;
   padding: 0;
+}
+
+section.images {
+  position: relative;
+  margin: 1rem;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  max-width: 100%;
+  gap: 1%;
+  object-fit: cover;
+}
+
+.dark-logo {
+  max-width: 140%;
+  margin-bottom: 2px;
 }
 
 .hero-banner {
@@ -77,41 +137,44 @@ html {
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
+  margin-bottom: 1.4rem;
 }
 
 .homepage-hero {
   position: absolute;
   left: -1.3rem;
-  top: 0px;  
+  top: 0px;
   z-index: -1;
-  overflow: scroll;
+  overflow: hidden;
+  width: 110%;
+}
+
+form {
+  width: 90%;
 }
 
 .input-field {
-  width: 22.5rem;
-  height: 36px;
+  position: relative;
 }
 
 .input-field > label {
   color: #ffffff;
   font-size: 1rem;
   cursor: text;
-  text-align: initial;
+  text-align: center;
 }
 
 #search {
-  padding-left: 0.4rem;
+  padding-left: 0.7rem;
+  width: 95%;
+  background: #00acc1;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
+  border-radius: 0.5rem;
 }
 
 .label-icon {
-  margin-left: 19.5rem;
+  margin-left: 87%;
   margin-top: 0.8rem;
-}
-
-#search {
-  background: #00ACC1;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
-  border-radius: 4px;
 }
 
 .homepage-elements {
@@ -121,14 +184,12 @@ html {
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
+  margin-bottom: 3rem;
 }
 
 img {
-  width: 110%;
+  width: 100%;
   object-fit: fill;
-  /* margin: 0;
-  padding: -5px; */
-  /* height: 100%; */
 }
 
 .row {
@@ -138,8 +199,6 @@ img {
 .btn {
   width: 150px;
   height: 60px;
-  /* margin-bottom: 1rem; */
   margin-top: 0.8rem;
-  /* padding-bottom: 1rem; */
 }
 </style>
